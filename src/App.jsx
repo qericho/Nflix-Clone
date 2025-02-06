aa82ca75cfe0f3c715e3cb6e6f4abee1b4ac0ac5
no_name <echstmr@gmail.com>
2025-02-06 11:20:12 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar.jsx"
-import Home from "./pages/home.jsx"
+import Home from "./pages/Home.jsx"
 import SignUp from "./pages/SignUp.jsx"
 import Login from "./pages/Login.jsx"
 

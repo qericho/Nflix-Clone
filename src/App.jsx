9cd0bfaf30925f5901d9725017d90b75065ed331
echstmr <stamariaecho@gmail.com>
2025-02-06 10:06:20 +0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/home"
-<<<<<<< HEAD
 import SignUp from "./pages/SignUp"
 import Login from "./pages/Login"
-=======
->>>>>>> 151f71d (Initial commit)
 
 const App = () => {
   return (
@@ -13,11 +10,8 @@ const App = () => {
         <Navbar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
-<<<<<<< HEAD
           <Route path="/signup" element={<SignUp/>}/>
           <Route path="/login" element={<Login/>}/>
-=======
->>>>>>> 151f71d (Initial commit)
         </Routes>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,53 +1,34 @@
-<<<<<<< HEAD
-import { Link } from "react-router-dom"
-=======
->>>>>>> 151f71d (Initial commit)
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="absolute w-full  z-[100]">
-        <div className="container p-2 md:p-4 mx-auto flex items-center justify-between">
-            {/* Logo */}
-            <div>
-<<<<<<< HEAD
-              <Link  to={'/'}>
-                <h1
-                 className="text-3xl md:text-5xl cursor-pointer
-                  text-red-600 font-bold">NETFLIX</h1>
-              </Link>
-            </div>
-            {/* Buttons */}
-            
-            <div>
-            <Link to={'./signup'}>
-                <button className="px-6 py-3 text-[16px] md:text-[18px]
-                 font-medium text-white cursor-pointer">
-                    Sign In</button>
-            </Link>
-            <Link to={'./login'}>
-=======
-                <h1
-                 className="text-3xl md:text-5xl cursor-pointer
-                  text-red-600 font-bold">NETFLIX</h1>
-            </div>
-            {/* Buttons */}
-            <div>
-                <button className="px-6 py-3 text-[16px] md:text-[18px]
-                 font-medium text-white cursor-pointer">
-                    Sign In</button>
->>>>>>> 151f71d (Initial commit)
-                <button
-                 className="px-6 py-2 bg-red-600 hover:bg-red-800
-                  text-[16px] md:text-[18px] font-medium text-white cursor-pointer
-                   rounded-md">Sign Up</button>
-<<<<<<< HEAD
-            </Link>
-=======
->>>>>>> 151f71d (Initial commit)
-            </div>
+    <nav className="absolute w-full z-[100]">
+      <div className="container p-2 md:p-4 mx-auto flex items-center justify-between">
+        {/* Logo */}
+        <div>
+          <Link to={'/'}>
+            <h1 className="text-3xl md:text-5xl cursor-pointer text-red-600 font-bold">NETFLIX</h1>
+          </Link>
         </div>
-    </nav>
-  )
-}
 
-export default Navbar
+        {/* Buttons */}
+        <div className="flex items-center space-x-4">
+          <Link to={'/login'}>
+            <button className="px-6 py-3 text-[16px] md:text-[18px] font-medium text-white cursor-pointer">
+              Sign In
+            </button>
+          </Link>
+
+          <Link to={'/signup'}>
+            <button
+              className="px-6 py-2 bg-red-600 hover:bg-red-800 text-[16px] md:text-[18px] font-medium text-white cursor-pointer rounded-md">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
